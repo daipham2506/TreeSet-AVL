040600584a49e366acb20995b19408ccca06b11b
Dai <daipham2506@gmail.com>
2018-12-30 22:07:03 +0700

    commit

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include<ctime>
+//#include<fstream>
+//#include <string>
+//#define max 1000000
+//#define max1 500000000
+//using namespace std;
+//
+//int main()
+//{
+//	ofstream fout;
+//	fout.open("input.txt");
+//	srand(time(0));
+//	for (int i = 0; i<max; i++) {
+//		double r = rand() % max1 + 1;
+//		fout << "a " << r << "\n";
+//	}
+//	fout.close();
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ TreeSet.h
@@ -0,0 +1,52 @@
+/* Copyright (C) 2018
+* Course: CO2003
+* Author: Rang Nguyen
+* Ho Chi Minh City University of Technology
+*/
+
+//#pragma once
+#include"AVLNode.h"
+class TreeSet
+{
+private:
+	AVLNode * root;
+	int count;
+
+protected:
+	void clearRec(AVLNode*root);
+	void insert(AVLNode* &root, int val);
+	AVLNode* del(AVLNode* &root, int val);
+	int height(AVLNode* root);
+	int updateBalance(AVLNode* root);
+	AVLNode* rotateLeft(AVLNode* &root);
+	AVLNode* rotateRight(AVLNode* &root);
+	void RightRight(AVLNode* &root);
+	void LeftLeft(AVLNode* &root);
+	void RightLeft(AVLNode* &root);
+	void LeftRight(AVLNode* &root);
+	int _contains_(AVLNode* root, int val);
+	int findMax(AVLNode* root);
+	int findMin(AVLNode* root);
+	AVLNode* _copy_(AVLNode* root,TreeSet* &result);
+	TreeSet* _subSet_(AVLNode* root, TreeSet* &tree, int fromVal, int toVal);
+public:
+	TreeSet();
+	~TreeSet();
+	void clear();
+	// print out the set in ascending order
+	friend ostream& operator<<(ostream& os, const TreeSet& t);
+
+	// YOUR TASKS START HERE	
+	int add(int val);
+	bool contains(int val);
+	void copy(const TreeSet& set);
+	int first();
+	int higher(int val);
+	int last();
+	int lower(int val);
+	int remove(int val);
+	TreeSet* subSet(int fromVal, int toVal);
+	int size();
+	// END HERE
+};
+
